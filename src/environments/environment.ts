2d1f12763688a21cc97baa71fcd603b203383cc8
Alex-Gombos <74148529+Alex-Gombos@users.noreply.github.com>
2023-04-28 22:59:24 +0300

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiYWxleGdvbWJvcyIsImEiOiJjbGgweXA1cXIweXcwM2Ryem03dGQ2bjU0In0.GJ6zQHvSa0m48YkuW5ZoiA'
+  }
+};
